bc4beae979c5cf5f2218bc0312448be00d0717c5
A.P. Liang <apandaliang@gmail.com>
2016-08-24 15:25:07 +0800

    Fixed the "Dave" input

--- src/Urnotdave.java
+++ src/Urnotdave.java
@@ -6,9 +6,15 @@ public class Urnotdave {
 		Scanner in = new Scanner(System.in);
 		System.out.println("Hello, my name is Hal. What is your name?");
 		String name = in.nextLine();
-		System.out.println("Hello " + name + ", would you like to hear a story?" );
+			if (name.equals("Dave")) {
+				System.out.println("Hello Dave."); 
+				System.out.println("We're going to reenact a scene from our film.");
+				}
+			else {
+				System.out.println("Hello " + name + ", would you like to hear a story?" );
+			}
 		
-	
+
 	
 	
 	
