8c6f80c95029b6fd35c2de896a8bae9e27c55741
A.P. Liang <apandaliang@gmail.com>
2016-08-24 15:04:32 +0800

    Created files

---
+++ src/Urnotdave.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Urnotdave {
+	
+	public static void main(String args[]) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Hello, my name is Hal. What is your name?");
+		String name = in.nextLine();
+		System.out.println("Hello " + name + ", would you like to hear a story?" );
+		
+	
+	
+	
+	
+	}
+}
